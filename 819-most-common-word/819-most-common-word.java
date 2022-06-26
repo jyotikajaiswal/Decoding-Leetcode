bed25c2a3e7a3b2d310de61a1c73101945abeb72
jyotikajaiswal <73827313+jyotikajaiswal@users.noreply.github.com>
2022-06-27 02:44:10 +0530

    Time: 3 ms (100.00%), Space: 42.3 MB (87.84%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -1,25 +1,44 @@
 class Solution {
-    public String mostCommonWord(String paragraph, String[] banned) {
-
-        // 1). replace the punctuations with spaces,
-        // and put all letters in lower case
-        String normalizedStr = paragraph.replaceAll("[^a-zA-Z0-9 ]", " ").toLowerCase();
-
-        // 2). split the string into words
-        String[] words = normalizedStr.split("\\s+");
-
-        Set<String> bannedWords = new HashSet();
-        for (String word : banned)
-            bannedWords.add(word);
-
-        Map<String, Integer> wordCount = new HashMap();
-        // 3). count the appearance of each word, excluding the banned words
-        for (String word : words) {
-            if (!bannedWords.contains(word))
-                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+    Set<String> banSet = new HashSet<>();
+    Map<String, Integer> map = new HashMap<>();
+    int max;
+    String res;
+    
+    public String mostCommonWord(String paragraph, String[] banned) 
+    {
+        for (String st : banned) banSet.add(st);
+        
+        StringBuilder sb = new StringBuilder("");
+        for (char ch : paragraph.toCharArray())
+        {
+            if (ch >= 'a' && ch <= 'z')
+                sb.append(ch);
+            else if (ch >= 'A' && ch <= 'Z')
+                sb.append((char) (ch + ' '));
+            else process(sb);
         }
-
-        // 4). return the word with the highest frequency
-        return Collections.max(wordCount.entrySet(), Map.Entry.comparingByValue()).getKey();
+        
+        process(sb);
+       
+        return res;
+    }
+        
+    public void process(StringBuilder sb)
+    {
+        if (sb.length() != 0)
+        {
+            String word = sb.toString().toLowerCase();
+            sb.setLength(0);
+            if (!banSet.contains(word)) 
+            {
+                int count = map.getOrDefault(word, 0);                
+                map.put(word, ++count);
+                if (count > max)
+                {
+                    max = count;
+                    res = word;
+                }
+            }
+        }
     }
 }
