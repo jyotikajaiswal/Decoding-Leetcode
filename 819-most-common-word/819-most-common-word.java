e989e61413cd12a67dbf47841a46d34375c5be83
jyotikajaiswal <73827313+jyotikajaiswal@users.noreply.github.com>
2022-06-27 03:26:07 +0530

    Time: 289 ms (10.14%), Space: 44.9 MB (21.31%) - LeetHub

--- 819-most-common-word/819-most-common-word.java
+++ 819-most-common-word/819-most-common-word.java
@@ -1,44 +1,32 @@
 class Solution {
-    Set<String> banSet = new HashSet<>();
-    Map<String, Integer> map = new HashMap<>();
-    int max;
-    String res;
-    
-    public String mostCommonWord(String paragraph, String[] banned) 
-    {
-        for (String st : banned) banSet.add(st);
-        
-        StringBuilder sb = new StringBuilder("");
-        for (char ch : paragraph.toCharArray())
-        {
-            if (ch >= 'a' && ch <= 'z')
-                sb.append(ch);
-            else if (ch >= 'A' && ch <= 'Z')
-                sb.append((char) (ch + ' '));
-            else process(sb);
+    public String mostCommonWord(String paragraph, String[] banned) {
+        HashMap<String, Integer> map = new HashMap<>();
+        String n = paragraph.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", " ");
+        String words[] = n.split(" ");
+        for(int i=0;i<words.length;i++){
+            String x = words[i];
+            if(!map.containsKey(x) && x!="")
+                map.put(x,1);
+            else if(x=="")
+                continue;
+            else
+                map.put(x,map.get(x)+1);
         }
-        
-        process(sb);
-       
-        return res;
-    }
-        
-    public void process(StringBuilder sb)
-    {
-        if (sb.length() != 0)
-        {
-            String word = sb.toString().toLowerCase();
-            sb.setLength(0);
-            if (!banSet.contains(word)) 
-            {
-                int count = map.getOrDefault(word, 0);                
-                map.put(word, ++count);
-                if (count > max)
-                {
-                    max = count;
-                    res = word;
-                }
+        for(int i=0;i<banned.length;i++){
+            String y = banned[i];
+            if(map.containsKey(y))
+                map.remove(y);
+        }
+        int max = 0;
+        String z="";
+        for(int i=0;i<words.length;i++){
+            if(map.containsKey(words[i])){
+            if(map.get(words[i])>max){
+                max = map.get(words[i]);
+                z = words[i];
+            }
             }
         }
+        return z;
     }
 }
