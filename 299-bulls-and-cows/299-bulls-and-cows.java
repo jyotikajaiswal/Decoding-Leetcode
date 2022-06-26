b0ea8010cfa17e862371a6a9bbccc4e5e80a0a51
jyotikajaiswal <73827313+jyotikajaiswal@users.noreply.github.com>
2022-06-27 04:48:18 +0530

    Time: 8 ms (76.20%), Space: 42.8 MB (56.85%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.java
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -1,21 +1,22 @@
 class Solution {
     public String getHint(String secret, String guess) {
-        int bulls = 0;
-        int cows = 0;
-        int[] sec = new int[10];
-        int[] gue = new int[10];
-        for (int i = 0; i < secret.length(); i ++) {
-            if (secret.charAt(i) == guess.charAt(i)) {
-                bulls ++;
+        int count =0,c=0;
+        int[] arr1 = new int[15];
+        int[] arr2 = new int[15];
+        for(int i=0;i<secret.length();i++){
+            char x = secret.charAt(i);
+            char y = guess.charAt(i);
+            if(secret.charAt(i) == guess.charAt(i)){
+                count++;
+                }
+            else{
+                arr1[x - '0']++;
+                arr2[y - '0']++;
             }
-            else {
-                sec[secret.charAt(i) - '0'] ++;
-                gue[guess.charAt(i) - '0'] ++;
             }
+        for(int i=0;i<arr1.length;i++){
+            c = c+ Math.min(arr1[i],arr2[i]);
         }
-        for (int i = 0; i < sec.length; i ++) {
-            cows += Math.min(sec[i], gue[i]);
-        }
-        return bulls + "A" + cows + "B";
+        return count + "A" + c + "B";
     }
 }
