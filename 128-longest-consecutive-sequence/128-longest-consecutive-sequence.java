6ffa7f7f7e52d1ea0d600b08a135f7640d48498d
jyotikajaiswal <73827313+jyotikajaiswal@users.noreply.github.com>
2022-06-28 03:12:11 +0530

    Time: 79 ms (47.90%), Space: 74 MB (56.92%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -27,36 +27,30 @@
 //     }
 // }
 class Solution {
-    public int longestConsecutive(int[] a1) {
-     	
-		HashMap<Integer,Boolean>lcs=new HashMap<>();
-		int n=a1.length;
-		for(int i=0;i<n;i++) {
-		lcs.put(a1[i],true);
-		}
-		
-		for(int i=0;i<n;i++) {
-		if(lcs.containsKey(a1[i]-1)) {
-		lcs.put(a1[i],false);
-		}
-		}
-		
-		int msp=0;
-		int ml=0;
-		for(int key:lcs.keySet()) {
-			if(lcs.get(key)==true) {
-				int sp=key; //start point of sequence
-				int l=1; //length of sequence
-				while(lcs.containsKey(sp+l)) {
-				l++;
-				}
-				if(l>ml) {
-					msp=sp;
-					ml=l;
-				}
-			}
-		}
-		
-    return  ml; 
+    public int longestConsecutive(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        
+        if(nums.length < 1){
+            return 0;
+        }
+        int maxLongest = 1;
+        int longest = 1;
+        
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], false);
+        }
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i] + 1) && !map.get(nums[i])){
+                longest++;
+                map.put(nums[i], !map.get(nums[i]));
+            }
+            else if(!map.containsKey(nums[i] + 1)){
+                maxLongest = Math.max(longest, maxLongest);
+                longest = 1;
+            }
+        }
+            
+        return maxLongest;
     }
 }
