077ce2cbc9950f4c08f623ce9442587ff185ad0a
jyotikajaiswal <73827313+jyotikajaiswal@users.noreply.github.com>
2022-07-18 01:10:55 +0530

    Time: 5 ms (78.24%), Space: 73.7 MB (52.57%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int obp = -prices[0];
-        int osp = 0;
-        for(int i=1;i<prices.length;i++){
-            int nbp = 0;
-            int nsp = 0;
-            if(osp-prices[i]>obp){
-                nbp = osp-prices[i];
-            }
-            else
-                nbp = obp;
-            if(prices[i]+obp-fee>osp){
-                nsp = prices[i]+obp-fee;
-            }
-            else
-                nsp = osp;
-            obp = nbp;
-            osp = nsp;
+        int len = prices.length, profit = 0, buy = prices[0];
+        for (int i = 1; i < len; i++) {
+            profit = Math.max(profit,  prices[i]- buy - fee);
+            buy = Math.min(buy, prices[i]-profit);
         }
-        return osp;
+        return profit;
+            // int nbp = 0;
+            // int nsp = 0;
+            // if(osp-prices[i]>obp){
+            //     nbp = osp-prices[i];
+            // }
+            // else
+            //     nbp = obp;
+            // if(prices[i]+obp-fee>osp){
+            //     nsp = prices[i]+obp-fee;
+            // }
+            // else
+            //     nsp = osp;
+            // obp = nbp;
+            // osp = nsp;  
     }
 }
